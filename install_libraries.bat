63ab917c8189050f1087cb48acd81972f6f615b7
lycanhunt3r <serginhohunter@gmail.com>
2021-04-26 04:55:39 -0300

    Update install_libraries.bat

--- install_libraries.bat
+++ install_libraries.bat
@@ -3,10 +3,10 @@ cls
 
 ECHO --------------------------------------
 ECHO Installing libraries, this might take a while
-ECHO  
+ECHO. 
 ECHO - Even if errors appear, wait until it's over and test if the bot is working properly, if not the case you should search what libraries are missing
 ECHO --------------------------------------
-ECHO  
+ECHO.  
 pip3 install cryptography
 pip3 install uuid
 pip3 install boto3
@@ -19,10 +19,10 @@ pip3 install psutil
 pip3 install ciso8601
 pip3 install numpy
 pip3 install talib
-ECHO  
+ECHO.  
 ECHO --------------------------------------
 ECHO All libraries finished!
-ECHO  
+ECHO.  
 ECHO - If the startbot.bat still do not work because of a missing library, search how to install it or try running this .bat again
 ECHO --------------------------------------
-ECHO  
+ECHO.  
